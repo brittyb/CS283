1eac797f10476c552a46754239a5eae73cc3f48b
brittyb <124463698+brittyb@users.noreply.github.com>
2025-02-15 20:30:23 -0500

    Add part 3 code

--- assignments/4-ShellP2/starter/dshlib.c
+++ assignments/4-ShellP2/starter/dshlib.c
@@ -8,6 +8,42 @@
 #include <sys/wait.h>
 #include "dshlib.h"
 
+int remove_whitespace(char *str)
+{
+	int start_index = 0;
+	int end_index = strlen(str) - 1;
+	int current_index = 0;
+	
+	while(str[start_index] == SPACE_CHAR)
+	{
+		start_index++;
+	}
+
+	while(str[end_index] == SPACE_CHAR)
+	{
+		end_index--;
+	}
+	int new_len = end_index + 1 - start_index;
+
+	memmove(str, str + start_index, new_len);
+	str[new_len] = '\0';
+	return 0;
+}
+
+void get_exe_and_args(char *cmd, command_list_t *clist)
+{
+	char *saveptr;
+	char *exe_token = strtok_r(cmd, " ", &saveptr);
+	char *arg_token = strtok_r(NULL, "", &saveptr);
+	 
+	//printf("exe: %s args: %s\n", exe_token, arg_token);
+	strcpy(clist->commands[clist->num].exe, exe_token);
+	if(arg_token != NULL)
+	{
+		strcpy(clist->commands[clist->num].args, arg_token);
+	}
+	
+}
 /*
  * Implement your exec_local_cmd_loop function by building a loop that prompts the 
  * user for input.  Use the SH_PROMPT constant from dshlib.h and then
@@ -53,9 +89,32 @@
  */
 int exec_local_cmd_loop()
 {
-    char *cmd_buff;
-    int rc = 0;
-    cmd_buff_t cmd;
+    memset(clist, 0, sizeof(command_list_t));
+    char * cmd_token = strtok(cmd_line, PIPE_STRING);
+    while(cmd_token != NULL)
+    {
+	
+	//printf("cmd token %s\n", cmd_token);
+	
+	if(clist->num >= CMD_MAX){
+		//printf("Too many commands\n");
+		return ERR_TOO_MANY_COMMANDS;
+	}
+	//printf("Before removing whitespace: %s\n", cmd_token);
+	remove_whitespace(cmd_token);	
+	//printf("After removing whitespace: %s\n", cmd_token);
+	if(strlen(cmd_token) == 0 || strlen(cmd_line) == 0){
+		return WARN_NO_CMDS;
+	}	
+	get_exe_and_args(cmd_token, clist);
+	if(clist->num >= CMD_MAX)
+	{
+		return ERR_TOO_MANY_COMMANDS;
+	}	
+	clist->num++;
+		
+	
+	cmd_token = strtok(NULL, PIPE_STRING);
 
     // TODO IMPLEMENT MAIN LOOP
 
